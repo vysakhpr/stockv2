e0406a600bb29c2304a79fcda481dd71597a8855
vysakhpr <my_mail_is_buzy@yahoo.com.au>
2014-02-08 21:55:09 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stockv2::Application.config.secret_token = 'fbab43c641ae8af9dd1701b9b522f45a296b7ad6f0cdf1c518ebd2b59d6fd4238c825b179fce5fb56f86738aa663decaa7b33cf8b2d724bf9918fe1e2587863c'
