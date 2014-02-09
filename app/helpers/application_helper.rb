7b55b30b8e73dfecb519d0a509204cb00a87d234
vysakhpr <my_mail_is_buzy@yahoo.com.au>
2014-02-09 10:48:20 +0530

    Implemented current user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
 	def signed_in?
 		admin_signed_in? #|| hod_signed_in? || lab_signed_in?
 	end	
+
+	def signed_in_user?
+		unless signed_in?
+			flash[:notice]="Please Sign In"
+			redirect_to root_url
+		end
+	end
+
+	def current_user
+		if admin_signed_in?
+		  Admin.find(session[:admin_id])
+		end	
+	end
 end
